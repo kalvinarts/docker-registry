8a15c068d5e178a8f5b32d30e03c662781ba2e09
Kalvin <kalvinarts@gmail.com>
2019-06-26 17:34:59 +0200

    Fixed delete-untagged.sh to use env vars

--- delete-untagged.sh
+++ delete-untagged.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker exec registry registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
+if [ ! -f .env ]; then
+  echo ".env file not found"
+  echo "copy .env.sample to .env and modify it to suit your environment"
+  exit 1
+fi
+
+source .env
+
+docker exec $REGISTRY_NAME registry garbage-collect /etc/docker/registry/config.yml --delete-untagged=true
